076a94f8b5e1ad693d8d98602f041719d67a59b2
kimtaehee829 <kimhanse000@gmail.com>
2026-05-27 00:17:57 +0900

    feat: 게시글 상세 조회, 수정, 삭제(CRUD) 기능 및 권한 검증 로직 추가

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListCreateView
+from .views import PostListCreateView, PostDetailView
 
 urlpatterns = [
     path('', PostListCreateView.as_view(), name='post_list_create'),
+    path('<int:post_id>/', PostDetailView.as_view(), name='post-detail'),
 ]
