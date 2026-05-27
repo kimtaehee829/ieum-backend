606ac3e8a09e2ab10b75b72d49fc98066e1458c6
kimtaehee829 <kimhanse000@gmail.com>
2026-05-27 23:54:42 +0900

    feat: 내가 쓴 글 모아보기 기능 추가

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer, ProfileSerializer
+from .serializers import UserSerializer, ProfileSerializer, MyPostListSerializer
+from posts.models import Post
 
 
 class SignupView(APIView):
@@ -36,3 +37,12 @@ class ProfileView(APIView):
                 status=status.HTTP_200_OK
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MyPostsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        posts = Post.objects.filter(author=request.user).order_by('-created_at')
+
+        serializer = MyPostListSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
